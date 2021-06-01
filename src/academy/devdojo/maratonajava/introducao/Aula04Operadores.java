d0d96fe70ab326af3ea6d5f2969f9645fca19c94
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-01 19:28:57 -0300

    16 - Operadores pt 02 - Relacionais

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -13,7 +13,24 @@ public class Aula04Operadores {
         System.out.println(soma);
 
         // divisão de números inteiros sempre resulta em um inteiro
-        double divisao = numero01 / numero02;
+        // pode usar casting, mas não é recomendável
+        double divisao = numero01 / (double) numero02;
         System.out.println(divisao);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVInte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10.0;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVInte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
     }
 }
