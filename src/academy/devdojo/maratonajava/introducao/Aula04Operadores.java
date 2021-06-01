318c0aa2448fd6066303f4e9626291300d434368
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-01 20:07:04 -0300

    19 - Operadores pt 05 - Atribuicao

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -47,5 +47,31 @@ public class Aula04Operadores {
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystationCinco ||
                 valorTotalContaPoupanca > valorPlaystationCinco;
         System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2; // 3600
+        bonus /= 2; // 1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        // incremento ++ e decremento --
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println(contador);
+
+        int contador2 = 0;
+        System.out.println(contador2++); // 0
+        System.out.println(contador2); // 1
+
+        int contador3 = 0;
+        System.out.println(++contador3); // 1
+        System.out.println(contador3); // 1
     }
 }
