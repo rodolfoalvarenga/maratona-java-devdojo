9895a6391cfbc1dfc86379928b23d2c926e5ea6a
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-25 11:33:48 -0300

    30 - Estruturas de Repeticao pt 04 - Exercicio Break

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+        // Dado o valor de um carro, descubra em quantas vezes pode ser parcelado
+        // Condição é que o valor da parcela tem que ser maior ou igual a 1000
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
