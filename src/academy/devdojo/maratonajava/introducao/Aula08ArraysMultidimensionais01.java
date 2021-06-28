2736ae7f79ca4bb499009f28ceebc15b74996b03
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-28 16:35:23 -0300

    37 - Arrays Multidimensionais pt 02 - Foreach

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -19,5 +19,13 @@ public class Aula08ArraysMultidimensionais01 {
             }
         }
 
+        System.out.println("----------");
+
+        for (int[] arrBase : dias) {
+            for (int num : arrBase) {
+                System.out.println(num);
+            }
+        }
+
     }
 }
