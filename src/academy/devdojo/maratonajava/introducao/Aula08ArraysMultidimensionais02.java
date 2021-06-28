7d8db9992700a1e3ee2dec674b5303428849dcef
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-28 16:59:30 -0300

    38 - Arrays Multidimensionais pt 03 - Inicializacao

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        // Variáveis de referência inicializam como null se não tiver valor
+        // int[][] arrayInt = new int[3][];
+
+        int[][] arrayInt = new int[3][];
+
+        int[] array = {1, 2, 3};
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = array;
+        arrayInt[2] = new int[]{1, 2, 3, 4, 5, 6};
+
+        for (int[] arrayBase : arrayInt) {
+            System.out.println("\n------");
+            for (int num : arrayBase) {
+                System.out.print(num + " ");
+            }
+        }
+
+        int[][] arrayInt2 = {{0, 0}, {1, 2, 3}, {1, 2, 3, 4, 5, 6}};
+
+        for (int[] arrayBase : arrayInt2) {
+            System.out.println("\n------");
+            for (int num : arrayBase) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
