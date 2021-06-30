946fa2a374ad1d2af30729547330af345a45946b
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-30 14:58:30 -0300

    48 - Orientacao Objetos - Metodos pt 05 - Parametros tipo primitivo

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -35,4 +35,12 @@ public class Calculadora {
         }
         System.out.println(num1 / num2);
     }
+
+    public void alteraDoisNumeros(int numero1, int numero2) {
+        numero1 = 99;
+        numero2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("numero1: " + numero1);
+        System.out.println("numero2: " + numero2);
+    }
 }
