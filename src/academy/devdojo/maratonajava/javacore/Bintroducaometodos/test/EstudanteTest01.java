d67c324bf1b14931ccb21225bc10adde30771786
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-06-30 15:37:00 -0300

    50 - Orientacao Objetos - Metodos pt 07 - Parametros tipo referencia pt 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -20,5 +20,8 @@ public class EstudanteTest01 {
 
         impressora.imprime(estudante01);
         impressora.imprime(estudante02);
+        System.out.println("##########");
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
     }
 }
