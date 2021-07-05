7522e9ce8b8b0c971c0b25229b0a5c80fedee034
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-07-05 15:43:16 -0300

    53 - Orientacao Objetos - Metodos pt 10 - Exercise

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    /* Crie uma classe Funcionario com os seguintes atributos
+
+    nome
+            idade
+    salario // três salários devem ser guardados
+
+    Crie dois métodos
+
+    1. Para imprimir os dados
+    2. Para tirar a média dos salários e imprimir o resultado
+     */
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Sanji";
+        funcionario.idade = 23;
+        funcionario.salarios = new double[]{1200, 987.32, 2000};
+
+        funcionario.imprime();
+    }
+}
