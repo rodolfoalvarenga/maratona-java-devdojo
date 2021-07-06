83ffd742db73bad66ca0d38d4614a83bda920a8d
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-07-06 11:54:53 -0300

    56 - Orientacao Objetos - Modificador de acesso private, get e set pt 03

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime() {
         System.out.println(this.nome);
@@ -24,8 +25,6 @@ public class Funcionario {
             return;
         }
 
-        double media = 0;
-
         for (double salario : salarios) {
             media += salario;
         }
@@ -33,5 +32,33 @@ public class Funcionario {
         media /= salarios.length;
 
         System.out.println("\nMédia salarial: " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
