f7c87fa5586313423d2157a9e691d967a69d8a63
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2021-08-09 17:04:54 -0300

    78 - Orientação Objetos - Modificador final pt 02 - Tipo referência

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -4,6 +4,7 @@ public class Carro {
 
     private String nome;
     public static final double VELOCIDADE_LIMITE;
+    public final Comprador COMPRADOR = new Comprador();
 
     static {
         VELOCIDADE_LIMITE = 250;
--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,13 +1,19 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTest01 {
 
     public static void main(String[] args) {
         Carro carro = new Carro();
+        Comprador comprador2 = new Comprador();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
-        System.out.println(carro.VELOCIDADE_LIMITE_2);
+        System.out.println(carro.COMPRADOR);
+
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
     }
 
 }
