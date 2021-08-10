a1cf8e77ade389e07ff1fbe7ccb106d6f64e3b67
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2021-08-10 17:56:34 -0300

    81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
@@ -3,18 +3,16 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
+import static academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.*;
+
 public class ClientTest01 {
 
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 
 }
