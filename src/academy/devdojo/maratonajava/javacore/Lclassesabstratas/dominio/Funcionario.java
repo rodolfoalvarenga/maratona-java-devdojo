37c7707fa17614c730b60c673486257e1d285acb
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2021-08-11 16:30:44 -0300

    85 - Orientação Objetos - Classes abstratas pt 02 - Métodos abstratos

--- src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -2,12 +2,21 @@ package academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio;
 
 public abstract class Funcionario {
 
+    /*
+    * Classe abstrata pode ter método concreto e/ou abstrato
+    * Porém, método abstrato só podem existir em classe abstrata
+    * e os métodos abstratos DEVEM ser implementados nas sub classes
+    * */
+
     protected String nome;
     protected double salario;
+
+    public abstract void calculaBonus();
 
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
 
 }
