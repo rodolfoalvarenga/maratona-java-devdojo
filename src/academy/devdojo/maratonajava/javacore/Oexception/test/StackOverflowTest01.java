3c4ca9ba4f579b97b91ac34fd8122ba3c0632e0a
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2021-09-17 11:31:09 -0300

    95 - Exceções pt 01 - Errors

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Oexception.test;
+
+public class StackOverflowTest01 {
+
+    // Error acontece na JVM e não pode ser arrumado em tempo de execução
+    public static void main(String[] args) {
+        recursividade();
+    }
+
+    // simulando StackOverflowError (estoura o limite de memória disponível)
+    public static void recursividade() {
+        recursividade();
+    }
+
+}
