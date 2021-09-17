c19fecb9b4e6775792ed48faaa26c0759a384c51
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2021-09-17 14:02:14 -0300

    96 - Exceções pt 02 - RuntimeException

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+
+    public static void main(String[] args) {
+        // Checked e Unchecked
+        // Checked -> filhas da classe Exception
+        // Unchecked -> filhas da classe RuntimeException, não faz validação para o código compilar
+
+//        // NullPointerException
+//        Object object = null;
+//        System.out.println(object.toString());
+
+//        // ArrayIndexOutOfBoundsException
+//        int [] nums = {1, 2};
+//        System.out.println(nums[2]);
+    }
+
+}
