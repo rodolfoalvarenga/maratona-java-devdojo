b17e3f2573f3fed5bce03cc8d47ec33c5957fabc
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2021-09-28 08:59:10 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -13,14 +13,30 @@ public class WrapperTest01 {
         char charP = 'W';
         boolean booleanP = false;
 
+        // Autoboxing -> faz Java transformar tipo primitivo em Wrapper
+        // Unboxing -> faz Java transformar Wrapper em tipo primitivo
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; // autoboxing
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 10D;
         Character charW = 'W';
         Boolean booleanW = false;
+
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        Integer intW3 = new Integer("2"); // depreciado a partir do Java 9
+        boolean verdadeiro = Boolean.parseBoolean("TruE"); // case insensitive
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetter('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.isUpperCase('a'));
+        System.out.println(Character.isLowerCase('A'));
 
     }
 
